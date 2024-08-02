a4ba47d24e2e65abacbacecb3d8ad106c2b11ff8
Stoyan <125189626+Stenlyy@users.noreply.github.com>
2024-08-02 10:30:56 +0200

    update tests

--- tests/ui/add-product.test.js
+++ tests/ui/add-product.test.js
@@ -1,6 +1,6 @@
 const { test, expect } = require('@playwright/test');
 
-test.only('Check add products page', async ({ page }) => {
+test('Check add products page', async ({ page }) => {
     await page.goto('http://localhost:8081/Add-Product');
     const form = await page.$('input');
     expect(form).toBeTruthy();
